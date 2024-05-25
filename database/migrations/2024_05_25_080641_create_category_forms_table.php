9460017405cdc59af0ac8f0afdaf8c150e58ec8f
Clyde <jah.clydeortega14@gmail.com>
2024-05-25 16:54:49 +0800

    dynamic categories and forms migrations

---
+++ database/migrations/2024_05_25_080641_create_category_forms_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+        });
+
+        Schema::create('forms', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+        });
+
+        Schema::create('category_forms', function (Blueprint $table) {
+            $table->foreignUuid('category_id')->constrained();
+            $table->foreignUuid('form_id')->constrained();
+
+            $table->primary(['category_id', 'form_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {   
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('forms');
+        Schema::dropIfExists('category_forms');
+    }
+};
