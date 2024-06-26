648f7dfd406d5e4cda5bedb07b03c38da33f78b5
Clyde <jah.clydeortega14@gmail.com>
2024-06-27 00:19:56 +0800

    bookings table

---
+++ app/Models/Booking.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuid;
+
+class Booking extends Model
+{
+    use HasFactory, Uuid;
+
+    protected $table = 'bookings';
+
+    protected $fillable = [
+        'uuid',
+        'category_id',
+        'rental_listing_id',
+        'booked_by',
+        'status',
+        'completed_at',
+        'pick_up_date',
+        'pick_up_time',
+        'pick_up_location',
+        'drop_off_date',
+        'drop_off_time',
+        'drop_off_location',
+        'is_rescheduled',
+        
+    ];
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function bookedBy()
+    {
+        return $this->belongsTo(User::class, 'booked_by');
+    }
+
+    public function rentalListing()
+    {
+        return $this->belongsTo(RentalListing::class, 'rental_listing_id');
+    }
+
+    public function bookingStatus()
+    {
+        return $this->belongsTo(BookingStatus::class, 'status');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,12 @@ use App\Http\Controllers\FormController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\WorkflowController;
+use App\Http\Controllers\LandingPageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome1', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [LandingPageController::class, 'index'])->name('landing.page.index');
 
 Route::get('/itemDetails/{uuid}', [RentalItemController::class, 'itemDetails'])->name('itemDetails');
 
