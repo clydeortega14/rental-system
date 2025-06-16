eebea3e2e5d8d3d5dd6e0b8e3a891803b5001fc6
Clyde <jah.clydeortega14@gmail.com>
2025-06-16 21:25:26 +0800

    dashboard

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -15,17 +15,17 @@ export default function Dashboard({ auth }: PageProps) {
             user={auth.user}
             header={
                 <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Dashboardss
+                    Dashboard
                 </h2>
             }
         >
             <Head title="Dashboard" />
-            <button
+            {/* <button
                 onClick={() => (window.location.href = "/UserProfile")}
                 className="px-4 py-2 bg-blue-600 text-white rounded"
             >
                 Go to User Profile
-            </button>
+            </button> */}
             <div className="py-12 min-h-screen">
                 <div className="max-w-8xl mx-auto sm:px-6 lg:px-8">
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-8 items-start">
