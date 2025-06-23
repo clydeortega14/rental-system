2b9275922e4afb94cd7d008c55e94c43469f4402
AngeloPapas <papasangelo97@gmail.com>
2025-06-23 14:02:25 +0800

    Created Middleware for Permissions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckPermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->alias([
+            'permission' => CheckPermission::class,
+        ]);
+        
         $middleware->appendToGroup('check-user-info', [
 
             \App\Http\Middleware\CheckUserInfo::class
