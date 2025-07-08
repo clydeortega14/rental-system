ceeb173b58e1293982d27125c0c930611197a268
Clyde <jah.clydeortega14@gmail.com>
2025-07-08 09:55:36 +0800

    package development in laravel.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     Yajra\Address\AddressServiceProvider::class,
+    Renthive\PaymentGateway\PaymentGatewayServiceProvider::class
 ];
