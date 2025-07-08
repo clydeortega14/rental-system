254d674da31756c2f66a721055093b1dab70164c
diza <dizajeanmaitim@coredev.ph>
2025-07-08 09:02:59 +0800

    added uuid to shops

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -4,17 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Uuid;
+use Illuminate\Support\Str;
 
 class Shop extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuid;
 
     protected $fillable = [
+        'uuid',
         'lessor_id',
         'name',
         'description',
         'location',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->uuid = Str::uuid();
+        });
+    }
 
     public function lessor()
     {
