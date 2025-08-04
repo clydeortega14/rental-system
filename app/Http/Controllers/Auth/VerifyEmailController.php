c513843e831020897c01003392f44359a6d3f4b8
Clyde <jah.clydeortega14@gmail.com>
2025-08-04 22:33:46 +0800

    check user info middleware

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -15,7 +15,7 @@ class VerifyEmailController extends Controller
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('landing.page.index', absolute: false).'?verified=1');
+            return redirect()->intended(route('completing.user', absolute: false).'?verified=1');
         }
 
         if ($request->user()->markEmailAsVerified()) {
