f2c80d3123dc1f1476b3421c41ed7d6b268cb7c2
Clyde <jah.clydeortega14@gmail.com>
2025-08-04 15:29:34 +0800

    stats

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-import { visualizer } from 'rollup-plugin-visualizer';
-
 export default defineConfig({
     plugins: [
-        visualizer(),
         laravel({
             input: 'resources/js/app.tsx',
             refresh: [
