3623f8fb6b7b5195fb5e69076ad59128398808d1
Clyde <jah.clydeortega14@gmail.com>
2025-09-01 16:19:02 +0800

    test

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -8,8 +8,8 @@ const echo = new Echo({
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wssPort: 443,
+    forceTLS: false,
     enabledTransports: ['ws', 'wss'],
     withCredentials: true,
 });
