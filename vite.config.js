977bd850c15d1751b3bd1e95d69725e596700078
clde <clyde.coredev@gmail.com>
2025-12-12 08:53:34 +0800

    configs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,19 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',     // allows external container access
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: 'localhost',
+            port: 80,
+            protocol: "ws"
+        },
+        watch: {
+            usePolling: true, // needed for Docker bind mounts
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
