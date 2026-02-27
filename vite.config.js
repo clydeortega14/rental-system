bd6517f63cca60027db687b3cfe3f57c6fbce5ca
clyde <clyde.coredev@gmail.com>
2026-02-27 17:47:28 +0800

    fixed merged conflict

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+<<<<<<< HEAD
 export default defineConfig(({command}) => ({
     base: '/build/',
     build: {
@@ -13,10 +14,16 @@ export default defineConfig(({command}) => ({
     },
     server: command === 'serve' ? {
         host: true,
+=======
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',     // allows external container access
+>>>>>>> 977bd85 (configs)
         port: 5173,
         strictPort: true,
         hmr: {
             host: 'localhost',
+<<<<<<< HEAD
             port: 5173,
             protocol: 'ws',
         },
@@ -24,6 +31,15 @@ export default defineConfig(({command}) => ({
             usePolling: true,
         },
     } : undefined,
+=======
+            port: 80,
+            protocol: "ws"
+        },
+        watch: {
+            usePolling: true, // needed for Docker bind mounts
+        },
+    },
+>>>>>>> 977bd85 (configs)
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
