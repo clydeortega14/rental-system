e0fde6a0d63b68912dcac18588ac99320fa75cdb
clyde <clyde.coredev@gmail.com>
2026-02-28 12:25:56 +0800

    modified vite config

--- vite.config.js
+++ vite.config.js
@@ -1,59 +1,43 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
-<<<<<<< HEAD
-export default defineConfig(({command}) => ({
-    base: '/build/',
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        sourcemap: false,
-        emptyOutDir: true,
-    },
-    server: command === 'serve' ? {
-        host: true,
-=======
-export default defineConfig({
-    server: {
-        host: '0.0.0.0',     // allows external container access
->>>>>>> 977bd85 (configs)
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: 'localhost',
-<<<<<<< HEAD
-            port: 5173,
-            protocol: 'ws',
+export default defineConfig(({mode, command}) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    const isServe  = command === 'serve';
+
+    return {
+        base: '/build/',
+        build: {
+            outDir: 'public/build',
+            manifest: true,
+            sourcemap: false,
+            emptyOutDir: true,
         },
-        watch: {
-            usePolling: true,
-        },
-    } : undefined,
-=======
-            port: 80,
-            protocol: "ws"
-        },
-        watch: {
-            usePolling: true, // needed for Docker bind mounts
-        },
-    },
->>>>>>> 977bd85 (configs)
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            refresh: [
-                'resources/js/Pages/**',
-                'resources/js/Components/**',
-            ],
-        }),
-        react(),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
-            
-        },
-    },
-}));
+        server: isServe ? {
+            host: '0.0.0.0',
+            port: Number(env.VITE_PORT ?? 5173),
+            strictPort: true,
+            hmr: {
+                host: env.VITE_HMR_HOST ?? 'localhost',
+                port: Number(env.VITE_PORT ?? 5173)
+            },
+        } : undefined,
+        plugins: [
+            laravel({
+                input: 'resources/js/app.tsx',
+                refresh: [
+                    'resources/js/Pages/**',
+                    'resources/js/Components/**',
+                ],
+            }),
+            react(),
+        ],
+        // resolve: {
+        //     alias: {
+        //         '@': path.resolve(__dirname, 'resources/js'),
+                
+        //     },
+        // },
+    }
+})
